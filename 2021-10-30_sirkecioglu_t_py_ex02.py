02b4cf824feda0a558b38dd8c00e6ce62a979cf9
TugceS <91740661+tugces7@users.noreply.github.com>
2021-10-30 22:34:16 +0200

    Add files via upload

---
+++ 2021-10-30_sirkecioglu_t_py_ex02.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# Applications of Accelerators Lecture
+# Python Exercise 2
+# Tugce Sirkecioglu 3301159
+# October 2021
+
+# Write a python class for a particle, don't forget the init, by giving Z and A. This class should have a method which gives the number of neutron back. Instantiate this class with an example.
+
+class Nuclide:
+    def __init__(self, protonnumber, massnumber):
+        self.Z = protonnumber
+        self.A = massnumber
+
+    def get_neutronnumber(self):
+        print (self.A-self.Z)
+        
+Ra207 = Nuclide(88, 207)
+
+Ra207.get_neutronnumber()
+
+
+
+
+
