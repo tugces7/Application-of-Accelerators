0b8ca985e8d80fe588c9a95e6b8506924ed4079c
TugceS <91740661+tugces7@users.noreply.github.com>
2021-12-16 17:57:03 +0100

    Add files via upload

---
+++ 21-12-21_sirkecioglu_t_ex03_11.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# Applications of Accelerators Lecture
+# Python Exercise 3
+# Tugce Sirkecioglu 3301159
+# December 2021
+
+# Write a python function with the name of "get_rigidity" to calculate the magnetic rigidity and put your code as usual on your GitHUB and provide the link here.
+
+
+# In[6]:
+
+
+# Data is taken from the Question 5.
+
+class Rigidity:
+    def __init__(self,  kineticenergy, restmass, lorentzfactor, chargestate):
+        self.ekin = kineticenergy
+        self.m0 = restmass
+        self.beta = lorentzfactor
+        self.Q = chargestate
+
+    def get_rigidity(self):
+        print ((((self.ekin/(self.m0*931.5))+1)*self.m0*1.661E-27*self.beta*2.997E8)/(self.Q*1.602E-19))
+        
+p7TeV = Rigidity(7000000, 1.007276,0.999999991, 1)
+
+p7TeV.get_rigidity()
+
+
+# In[ ]:
+
+
+
+
---
+++ 21-12-21_sirkecioglu_t_ex03_12.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# Applications of Accelerators Lecture
+# Python Exercise 3
+# Tugce Sirkecioglu 3301159
+# December 2021
+
+# From the accelerator department, you received some data (see the attached data file: 2016-07-11_ipm_data.txt) from an ionisation beam profile monitor (IPM). Read the file and plot it in blue color on a black grid background. The mark the maximum with a red dot! Code and plot (PNG) as usual on GitHUB.
+
+
+# In[41]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('2016-07-11_ipm_data.txt', 'float')
+
+plt.plot(data, color='blue', linestyle='solid', linewidth = 0.5)
+
+plt.grid(axis='both', color='black', linestyle='-', linewidth=0.1)
+
+ymax = max(data)
+xpos = np.where(data == ymax)
+xmax = xpos
+
+plt.plot( xmax, ymax, 'ro', markersize=2.5)
+ 
+plt.savefig("2016-07-11_ipm_data_plot.png", dpi=250)
+
+
+# In[ ]:
+
+
+
+
