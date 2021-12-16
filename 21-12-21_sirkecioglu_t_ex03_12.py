74f4c992505f5e2899435e19e00db89e7e826e36
TugceS <91740661+tugces7@users.noreply.github.com>
2021-12-16 17:58:25 +0100

    Update 21-12-21_sirkecioglu_t_ex03_12.py

--- 21-12-21_sirkecioglu_t_ex03_12.py
+++ 21-12-21_sirkecioglu_t_ex03_12.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
 # Applications of Accelerators Lecture
 # Python Exercise 3
 # Tugce Sirkecioglu 3301159
 # December 2021
 
 # From the accelerator department, you received some data (see the attached data file: 2016-07-11_ipm_data.txt) from an ionisation beam profile monitor (IPM). Read the file and plot it in blue color on a black grid background. The mark the maximum with a red dot! Code and plot (PNG) as usual on GitHUB.
-
-
-# In[41]:
-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,10 +24,3 @@ xmax = xpos
 plt.plot( xmax, ymax, 'ro', markersize=2.5)
  
 plt.savefig("2016-07-11_ipm_data_plot.png", dpi=250)
-
-
-# In[ ]:
-
-
-
-
